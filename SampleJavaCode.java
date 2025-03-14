a8ecd23ff6095715ebaf7f22cbcd2eea60000bb1
NareshAutomationTesting <automationuser500@gmail.com>
2025-03-14 22:25:12 +0530

    Update SampleJavaCode.java

--- SampleJavaCode.java
+++ SampleJavaCode.java
@@ -1,5 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class SampleJavaCode {
-    public static void main(String[] args) {
-        System.out.println("Hello, Jenkins! Java program is running from GitHub.");
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void searchGoogle() {
+        driver.get("https://www.google.com");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Jenkins Selenium Test");
+        searchBox.submit();
+        Assert.assertTrue(driver.getTitle().contains("Jenkins Selenium Test"));
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
