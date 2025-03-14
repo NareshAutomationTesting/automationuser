62b0671a1e4e9a44d045d026f99acfa724d0d1dd
NareshAutomationTesting <automationuser500@gmail.com>
2025-03-14 22:21:33 +0530

    Update SampleJavaCode.java

--- SampleJavaCode.java
+++ SampleJavaCode.java
@@ -1,4 +1,4 @@
-public class HelloJenkins {
+public class SampleJavaCode {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins! Java program is running from GitHub.");
     }
