c57387b0d646108258edb5e769c62de2447c9e7e
Naresh2033 <naresh2033@gmail.com>
2025-03-15 13:01:27 +0530

    commit

--- JenkinsRunDemo/src/test/java/JenkinsDemoRun.java
+++ JenkinsRunDemo/src/test/java/JenkinsDemoRun.java
@@ -1,12 +1,10 @@
-package test.java.java;
+package test.java;
 import org.testng.annotations.Test;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class JenkinsDemoRun {
