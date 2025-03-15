a6ecab6506de5eee316dbda73b09e3a2f39ded80
Naresh2033 <naresh2033@gmail.com>
2025-03-15 12:50:41 +0530

    commit

--- JenkinsRunDemo/src/test/java/java/JenkinsDemoRun.java
+++ JenkinsRunDemo/src/test/java/java/JenkinsDemoRun.java
@@ -1,4 +1,7 @@
 package test.java.java;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -6,17 +9,23 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class JenkinsDemoRun {
+	private static final Logger logger = LogManager.getLogger(JenkinsDemoRun.class);
 
 	@Test
 	public void jenkinsTest() {
 
-		
-		        WebDriverManager.chromedriver().setup();
-		        WebDriver driver = new ChromeDriver();
-		        driver.manage().window().maximize();
-		        driver.get("https://www.saucedemo.com/");
-		        driver.quit();
-		    
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
+		driver.quit();
+
+		logger.debug("This is a DEBUG message");
+		logger.info("This is an INFO message");
+		logger.warn("This is a WARN message");
+		logger.error("This is an ERROR message");
+		logger.fatal("This is a FATAL message");
+
 	}
 
 }
