562aa2c21d1194e787b201c3a77e18ad698aa329
Naresh2033 <naresh2033@gmail.com>
2025-03-15 10:27:24 +0530

    commit

---
+++ JenkinsRunDemo/src/test/java/JenkinsDemoRun.java
@@ -0,0 +1,22 @@
+package test.java;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JenkinsDemoRun {
+
+	@Test
+	public void jenkinsTest() {
+
+		
+		        WebDriverManager.chromedriver().setup();
+		        WebDriver driver = new ChromeDriver();
+		        driver.manage().window().maximize();
+		        driver.get("https://www.saucedemo.com/");
+		        driver.quit();
+		    
+	}
+
+}
