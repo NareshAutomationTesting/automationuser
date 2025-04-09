d5a187671ce7b7f8729cfb3449e1f6f26f33c05e
NareshAutomationTesting <automationuser500@gmail.com>
2025-04-09 17:48:09 +0530

    Update LoginTest.java

--- JenkinsRunDemo/src/test/LoginTest.java
+++ JenkinsRunDemo/src/test/LoginTest.java
@@ -1,28 +1,7 @@
 package test;
+public class LoginTest {
+	public static void main(String args[]) {
+	System.out.println("Welcome to Login Test class");
+	}
 
-import org.testng.Assert;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
-import base.BaseTest;
-import pages.LoginPage;
-import utils.ExcelReader;
-import utils.Utils;
-
-public class LoginTest extends BaseTest {
-	
-	 @Test(dataProvider = "loginData")
-	    public void testLogin(String username, String password) {
-	        LoginPage loginPage = new LoginPage(driver);
-	        loginPage.login(username, password);
-	        boolean isLoggedIn = loginPage.isLoggedIn();
-	        Assert.assertTrue(false, "Login failed for user: " + username);
-	        updateReport(isLoggedIn);
-	    }
-
-    
-    @DataProvider(name = "loginData")
-    public Object[][] getLoginData() {
-        return ExcelReader.readExcelData("test-data/LoginData.xlsx", "Sheet1");
-    }
 }
